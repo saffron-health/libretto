034861dc7077d930abccfbe0423e6639bf65add7
Tanishq Kancharla <tanishqkancharla3@gmail.com>
2026-06-18 10:14:35 -0700

    Add Stagehand comparison page

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "wouter";
 import { HomePage } from "./HomePage";
 import { BlogIndexPage, BlogPostPage } from "./blog/BlogPage";
 import { BrowserUsePage } from "./vs/BrowserUsePage";
+import { StagehandPage } from "./vs/StagehandPage";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Route path="/blog" component={BlogIndexPage} />
       <Route path="/blog/:slug">{(params) => <BlogPostPage slug={params.slug ?? ""} />}</Route>
       <Route path="/vs/browser-use" component={BrowserUsePage} />
+      <Route path="/vs/stagehand" component={StagehandPage} />
       <Route path="*" component={HomePage} />
     </Switch>
   );
